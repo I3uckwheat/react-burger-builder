d94a1e1979a41b9ed1498d2f5f2d7dcd1c7f06c7
Briggs Elsperger <briggse@protonmail.com>
2018-05-07 20:46:06 -0500

    adds side drawer

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link='/' active>Burger Bulder</NavigationItem>
-    <NavigationItem link='/'>Burger Bulder</NavigationItem>
+    <NavigationItem link='/'>Order Now</NavigationItem>
   </ul>
 
 );
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,9 +6,11 @@ import classes from './Toolbar.css';
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
-    <Logo />
-    <nav>
+    <div onClick={props.open}>MENU</div>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
       <NavigationItems />
     </nav>
   </header>
